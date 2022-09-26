17a5abaaf4cbf9862a8ebf7942f4ec0db83d2ed7
Roman Hryshchuk <rhryshchik3332@gmail.com>
2022-09-26 14:25:39 +0300

    props added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: "Ihor", salary: 800, increase: false},
+        {name: "Olha", salary: 1500, increase: true},
+        {name: "Roman", salary: 2000, increase: false}
+    ];
+
     return (
         <div className="app">
             <AppInfo />
@@ -16,7 +23,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList />
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     )
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,18 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
+
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({ data }) => {
+    
+    const elements = data.map(item => {
+        return(
+            <EmployeesListItem {...item} /> //{...item} це те ж саме що name={item.name} salary={item.salary}
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem />
-            <EmployeesListItem />
-            <EmployeesListItem />
+            {elements}
         </ul>
     )
 }
